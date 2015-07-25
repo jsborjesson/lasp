ccd61a66450617952cfe9defb6ac7c5ab05e7de3
Jimmy Börjesson <lagginglion@gmail.com>
2015-07-25 15:37:02 +0200

    Refactor eval

--- lib/eval.rb
+++ lib/eval.rb
@@ -9,19 +9,21 @@ module Lasp
   end
 
   def eval(ast, env = global_env)
-    if Array === ast
-      if ast.first == :def
-        _, key, value = *ast
-        env[key] = value
-      else
-        head, *tail = *ast
-        fn = env[head]
-        fn.(env, *tail.map { |form| eval(form) })
-      end
-    elsif Symbol === ast
-      env[ast]
+    case ast
+    when Array  then eval_form(ast, env)
+    when Symbol then env[ast]
+    else ast
+    end
+  end
+
+  def eval_form(ast, env)
+    if ast.first == :def
+      _, key, value = *ast
+      env[key] = value
     else
-      ast
+      head, *tail = *ast
+      fn = env[head]
+      fn.(env, *tail.map { |form| eval(form) })
     end
   end
 end
