17fa951633a841d7fc0d4fe427d8fc260f03977d
Jimmy Börjesson <lagginglion@gmail.com>
2015-07-26 15:38:52 +0200

    Format stdlib

--- lib/corelib.rb
+++ lib/corelib.rb
@@ -1,15 +1,15 @@
 module Lasp
   CORELIB = {
-    :+   => -> (_, *args) { args.reduce(:+) },
-    :-   => -> (_, *args) { args.reduce(:-) },
-    :*   => -> (_, *args) { args.reduce(:*) },
-    :/   => -> (_, *args) { args.reduce(:/) },
-    :"=" => -> (_, *args) { args.uniq.count == 1 },
-    :list => -> (_, *items) { items },
-    :head => -> (_, list) { list.first },
-    :tail => -> (_, list) { list.drop(1) },
-    :cons => -> (_, item, list) { [item] + list },
-    :not  => -> (_, arg) { !arg },
-    :println => -> (_, output) { puts output }
+    :+       => -> (_, *args)      { args.reduce(:+)      },
+    :-       => -> (_, *args)      { args.reduce(:-)      },
+    :*       => -> (_, *args)      { args.reduce(:*)      },
+    :/       => -> (_, *args)      { args.reduce(:/)      },
+    :"="     => -> (_, *args)      { args.uniq.count == 1 },
+    :list    => -> (_, *args)      { args                 },
+    :head    => -> (_, list)       { list.first           },
+    :tail    => -> (_, list)       { list.drop(1)         },
+    :cons    => -> (_, item, list) { [item] + list        },
+    :not     => -> (_, arg)        { !arg                 },
+    :println => -> (_, output)     { puts output          },
   }
 end
