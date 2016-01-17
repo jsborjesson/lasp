be43c1d2f48ee9a9d924816ee8f60b5914bbb955
Jimmy Börjesson <alcesleo@gmail.com>
2016-01-17 21:05:50 +0100

    Break out all the special forms to functions
    The big nasty case-statement looks much prettier now.

--- lib/lasp/eval.rb
+++ lib/lasp/eval.rb
@@ -16,22 +16,36 @@ module Lasp
     head, *tail = *form
 
     case head
-    when :def
-      key, value = tail
-      env[key] = Lasp::eval(value, env)
-    when :fn
-      params, func = tail
-      -> (*args) { Lasp::eval(func, env.merge(Hash[params.zip(args)])) }
-    when :do
-      tail.map { |form| Lasp::eval(form, env) }.last
-    when :if
-      conditional, true_form, false_form = tail
-      Lasp::eval(conditional, env) ? Lasp::eval(true_form, env) : Lasp::eval(false_form, env)
-    when Proc
-      head.(*tail)
-    else
-      fn = Lasp::eval(head, env)
-      fn.(*tail.map { |form| Lasp::eval(form, env) })
+    when :def then def_special_form(tail, env)
+    when :fn  then fn_special_form(tail, env)
+    when :do  then do_special_form(tail, env)
+    when :if  then if_special_form(tail, env)
+    when Proc then head.(*tail)
+    else call_function(head, tail, env)
     end
+  end
+
+  def call_function(symbol, args, env)
+    fn = Lasp::eval(symbol, env)
+    fn.(*args.map { |form| Lasp::eval(form, env) })
+  end
+
+  def def_special_form(form, env)
+    key, value = form
+    env[key] = Lasp::eval(value, env)
+  end
+
+  def fn_special_form(form, env)
+    params, func = form
+    -> (*args) { Lasp::eval(func, env.merge(Hash[params.zip(args)])) }
+  end
+
+  def do_special_form(form, env)
+    form.map { |form| Lasp::eval(form, env) }.last
+  end
+
+  def if_special_form(form, env)
+    conditional, true_form, false_form = form
+    Lasp::eval(conditional, env) ? Lasp::eval(true_form, env) : Lasp::eval(false_form, env)
   end
 end
